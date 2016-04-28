b50ee34830b7c7b7ee7300c9f85d7e5fdce518a3
Mads-Strandbygaard <mads.strandbygaard.jakobsen@gmail.com>
2016-04-28 11:26:31 +0200

    added bg variations

--- public_html/bootstrap/js/games_lobby.js
+++ public_html/bootstrap/js/games_lobby.js
@@ -1,21 +1,26 @@
 var username = $('.squad-self-name').text();
 var queueTimes = []
 
-function GamesLobby_selectRole() {
-    if ($(this).hasClass('other-taken')){
-    }else{
-    queueTimes.push($(this).siblings('.role-name').children('.queue-est').text());
-    if (queueTimes.length > 2) {
-        queueTimes.shift()
-    };
-    $('.self-taken').children('.role-name').children('.queue-est').text(queueTimes[0]);
+setTimeout(function () {
+    $('#support').addClass('role-taken other-taken');
+    $('#support').siblings('.role-name').find('.queue-est').text('AX.Aeon.피자')
+}, 8200);
 
-    $('.self-taken').children('.role').removeClass('role-taken');
-    $('.self-taken').removeClass('self-taken');
-    $(this).addClass('role-taken');
-    $(this).parent().addClass('self-taken');
-    $(this).find('.role-taken-img').attr('src');
-    $(this).siblings('.role-name').children('.queue-est').text(username);
-}};
+function GamesLobby_selectRole() {
+    if ($(this).hasClass('other-taken')) {} else {
+        queueTimes.push($(this).siblings('.role-name').children('.queue-est').text());
+        if (queueTimes.length > 2) {
+            queueTimes.shift()
+        };
+        $('.self-taken').children('.role-name').children('.queue-est').text(queueTimes[0]);
+
+        $('.self-taken').children('.role').removeClass('role-taken');
+        $('.self-taken').removeClass('self-taken');
+        $(this).addClass('role-taken');
+        $(this).parent().addClass('self-taken');
+        $(this).find('.role-taken-img').attr('src');
+        $(this).siblings('.role-name').children('.queue-est').text(username);
+    }
+};
 
 $('.role').click(GamesLobby_selectRole);

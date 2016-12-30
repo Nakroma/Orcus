472ed9081845faa242b367aa15a305730cec222b
Nakroma <tarackobarack@gmail.com>
2016-12-30 02:22:30 +0100

    Added lobby publish/subscription

--- imports/api/lobby.js
+++ imports/api/lobby.js
@@ -6,3 +6,10 @@ import { Schemas } from './_schemas.js';
 // Create lobby collection
 export const Lobbies = new Mongo.Collection('lobbies');
 Lobbies.attachSchema(Schemas.Lobbies);
+
+// Publish
+if (Meteor.isServer) {
+    Meteor.publish('lobbies', function lobbyPublication() {
+        return Lobbies.find();
+    });
+}
--- imports/ui/lobby/lobby.js
+++ imports/ui/lobby/lobby.js
@@ -1,1 +1,9 @@
+import { Lobbies } from '../../api/lobby.js';
+import { Meteor } from 'meteor/meteor';
+
 import './lobby.html';
+
+/* Created */
+Template.partChat.onRendered(function () {
+    Meteor.subscribe('lobbies');
+});
